78deaba5e78ca4cced95aec51a36501d3a123c10
Citizshrestha <citizshresthaa@gmail.com>
2025-01-23 17:26:00 +0545

    "Added some functionalities in side-bar"

--- 5)EcommerceAdvanceFiltering/src/Sidebar/Category/Category.jsx
+++ 5)EcommerceAdvanceFiltering/src/Sidebar/Category/Category.jsx
@@ -1,36 +1,32 @@
 import "./Category.css";
 import Input from "../../components/Input";
 
-function Category() {
+function Category({handleChange}) {
   return (
     <div>
       <h2 className="sidebar-title">Category</h2>
 
       <div>
         <label className="sidebar-label-container">
-          <input  type="radio" value="" name="test" />
+          <input onChange={handleChange}  type="radio" value="" name="test" />
           <span className="checkmark"></span>All
         </label>
-        <Input
-          
+        <Input handleChange={handleChange}
           value="sneakers"
           title="Sneakers"
           name="test"
         />
-        <Input
-          
+        <Input handleChange={handleChange}
           value="flats"
           title="Flats"
           name="test"
         />
-        <Input
-          
+        <Input  handleChange={handleChange}
           value="sandals"
           title="Sandals"
           name="test"
         />
-        <Input
-          
+        <Input handleChange={handleChange}
           value="heels"
           title="Heels"
           name="test"
--- 5)EcommerceAdvanceFiltering/src/Sidebar/Colors/Colors.jsx
+++ 5)EcommerceAdvanceFiltering/src/Sidebar/Colors/Colors.jsx
@@ -1,17 +1,52 @@
 import "./Colors.css";
 import Input from "../../components/Input";
 
-const Colors = () => {
+const Colors = ({handleChange}) => {
   return (
     <>
       <div>
         <h2 className="sidebar-title color-title">Colors</h2>
-       <Input/>
-       <Input/>
-       <Input/>
-       <Input/>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange}  type="radio" value="" name="test" />
+          <span className="checkmark all"></span>All
+        </label>
 
-         
+        <Input
+          handleChange={handleChange}
+          value="black"
+          title="Black"
+          color="black"
+          name="test1"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value="red"
+          title="Red"
+          color="red"
+          name="test1"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value="green"
+          title="Green"
+          color="green"
+          name="test1"
+        />
+
+         <label className="sidebar-label-container">
+          <input onChange={handleChange}  type="radio" value="white" name="test1" />
+
+          <span className="checkmark" 
+          style={{
+            background:"white", 
+            border:"2px solid black"
+            }}></span>White
+
+        </label>
+
+
       </div>
     </>
   );
