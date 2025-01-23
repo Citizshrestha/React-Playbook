6bc6ffc4ef0c55ba664d2f27cb02906d4ec91005
Citizshrestha <citizshresthaa@gmail.com>
2025-01-23 17:47:55 +0545

    "Sidebar functionality completed"

--- 5)EcommerceAdvanceFiltering/src/products/Products.jsx
+++ 5)EcommerceAdvanceFiltering/src/products/Products.jsx
@@ -3,21 +3,11 @@ import  './Products.css'
 import Card from '../components/Card'
 
 
-const Products = () => {
+const Products = ({result}) => {
   return (
     <div>
         <section className="card-container"> 
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
+           {result}
         </section>
     </div>
   )
